4d5cdecccc1988fc89cd9d00fc9b7db417c4b928
ekomeisaac-1 <fomicyear3sd6@gmail.com>
2026-03-10 13:54:23 +0100

    Fix Django secret key

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "A4kf-wdj28dm.fc.eflxo2.di22,13.1o3.240dscoaimsfdrsdd,oew32sadf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
